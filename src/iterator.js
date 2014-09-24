47d5e8d0190b89bc89d43e7f784e7225f10753f4
Daniel Cousens <github@dcousens.com>
2014-09-24 22:02:17 +1000

    AddressIterator: adds impl and tests

---
+++ src/iterator.js
@@ -0,0 +1,49 @@
+function AddressIterator(hdNode, k) {
+  k = k || 0
+
+  // k-indexed address array
+  this.addresses = []
+
+  // address-indexed k map
+  this.map = {}
+
+  this.hdNode = hdNode
+  this.k = k - 1
+
+  // iterate to k:0
+  this.next()
+}
+
+AddressIterator.prototype.get = function() {
+  return this.addresses[this.addresses.length - 1]
+}
+
+AddressIterator.prototype.next = function() {
+  var address = this.peek()
+  this.k += 1
+
+  // address indexed k map
+  this.map[address] = this.k
+
+  // k indexed address array
+  this.addresses.push(address)
+}
+
+AddressIterator.prototype.peek = function() {
+  var xpub = this.hdNode.derive(this.k + 1)
+
+  return xpub.getAddress().toString()
+}
+
+AddressIterator.prototype.indexes = function(addresses) {
+  return addresses.filter(function(address) {
+    return (address in this.map)
+  }, this).map(function(address) {
+    return {
+      address: address,
+      k: this.map[address]
+    }
+  }, this)
+}
+
+module.exports = AddressIterator
---
+++ test/iterator.js
@@ -0,0 +1,111 @@
+var assert = require('assert')
+var bitcoinjs = require('bitcoinjs-lib')
+
+var AddressIterator = require('../src/iterator')
+
+describe('AddressIterator', function() {
+  var derived, hdNode
+
+  beforeEach(function() {
+    var seed = new Buffer(16)
+    seed.fill(1)
+
+    hdNode = bitcoinjs.HDNode.fromSeedBuffer(seed)
+
+    derived = []
+    for (var i = 0; i < 20; ++i) derived.push(hdNode.derive(i).getAddress().toString())
+  })
+
+  describe('constructor', function() {
+    it('defaults to k=0', function() {
+      var iter = new AddressIterator(hdNode)
+
+      assert.equal(iter.addresses.length, 1)
+      assert.equal(iter.addresses[0], derived[0])
+    })
+
+    it('can start other k offsets', function() {
+      var iter = new AddressIterator(hdNode, 5)
+
+      assert.equal(iter.addresses.length, 1)
+      assert.equal(iter.addresses[0], derived[5])
+    })
+  })
+
+  describe('get', function() {
+    var iter
+    beforeEach(function() {
+      iter = new AddressIterator(hdNode, 3)
+    })
+
+    it('returns the last address', function() {
+      iter.next()
+
+      assert.equal(iter.addresses.length, 2)
+      assert.equal(iter.get(), iter.addresses[1])
+    })
+  })
+
+  describe('next', function() {
+    var iter
+    beforeEach(function() {
+      iter = new AddressIterator(hdNode, 3)
+    })
+
+    it('iterates to the next address', function() {
+      assert.equal(iter.get(), derived[3])
+      iter.next()
+
+      assert.equal(iter.get(), derived[4])
+    })
+
+    it('adds the address to .addresses', function() {
+      assert.equal(iter.addresses.length, 1)
+      iter.next()
+
+      assert.equal(iter.addresses.length, 2)
+    })
+  })
+
+  describe('peek', function() {
+    var iter
+    beforeEach(function() {
+      iter = new AddressIterator(hdNode, 3)
+    })
+
+    it('shows the next address without mutation', function() {
+      assert.equal(iter.get(), derived[3])
+      var result = iter.peek()
+
+      assert.equal(result, derived[4])
+      assert.equal(iter.get(), derived[3])
+    })
+  })
+
+  describe('indexes', function() {
+    var iter
+    beforeEach(function() {
+      iter = new AddressIterator(hdNode, 0)
+
+      for (var j = 0; j < 20; ++j) iter.next()
+    })
+
+    it('finds the k-indexes for an array of addresses', function() {
+      var ks = iter.indexes(derived.slice(16))
+      var expected = derived.map(function(address, k) {
+        return { address: address, k: k }
+      })
+
+      assert.deepEqual(ks, expected.slice(16))
+    })
+
+    it('skips addresses not found', function() {
+      var ks = iter.indexes(['foobar', 'fizzbuzz', '1DZcPM5RbSpoZdPif951fdTMNMgf7GXSUZ'])
+
+      assert.deepEqual(ks, [{
+        address: '1DZcPM5RbSpoZdPif951fdTMNMgf7GXSUZ',
+        k: 4
+      }])
+    })
+  })
+})
