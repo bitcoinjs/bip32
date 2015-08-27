0c2419e3afb2e703a30ef5f781bd1fa6d38540e3
Daniel Cousens <github@dcousens.com>
2015-08-27 12:56:27 +1000

    chain: support lazy derivation, adds new methods
    adds .derive
    adds .neutered
    adds .escalated
    replaces .indexOf with .find

--- src/chain.js
+++ src/chain.js
@@ -1,45 +1,66 @@
+var xtend = require('xtend')
+
 function Chain (node, k) {
   k = k || 0
 
   this.addresses = []
-  this.k = k - 1
+  this.k = k
   this.map = {}
   this.node = node
-
-  // iterate to k:0
-  this.next()
 }
 
 Chain.prototype.get = function () {
+  if (this.addresses.length === 0) {
+    var address = this.derive(this.k).getAddress().toString()
+    this.map[address] = this.k
+    this.addresses.push(address)
+
+    return address
+  }
+
   return this.addresses[this.addresses.length - 1]
 }
 
-Chain.prototype.indexOf = function (address) {
-  return (address in this.map) ? this.map[address] : -1
+Chain.prototype.find = function (address) {
+  return this.map[address]
+}
+
+Chain.prototype.derive = function (k) {
+  return this.node.derive(k)
+}
+
+Chain.prototype.escalated = function (node) {
+  var chain = new Chain(node)
+
+  chain.addresses = chain.addresses.concat()
+  chain.k = this.k
+  chain.map = xtend({}, this.map)
+
+  return chain
+}
+
+Chain.prototype.neutered = function () {
+  var chain = new Chain(this.node.neutered())
+
+  chain.addresses = chain.addresses.concat()
+  chain.k = this.k
+  chain.map = xtend({}, this.map)
+
+  return chain
 }
 
 Chain.prototype.next = function () {
-  var address = this.peek()
+  var address = this.derive(this.k + 1).getAddress().toString()
+
   this.k += 1
-
-  // address indexed k map
   this.map[address] = this.k
-
-  // k indexed address array
   this.addresses.push(address)
 
-  return this.get()
-}
-
-Chain.prototype.peek = function () {
-  var xpub = this.node.derive(this.k + 1)
-
-  return xpub.getAddress().toString()
+  return address
 }
 
 Chain.prototype.pop = function () {
   var address = this.addresses.pop()
-
   delete this.map[address]
   this.k -= 1
 
