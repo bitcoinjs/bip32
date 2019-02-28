0df550ad047210fe5cfcc189bb78db645f2f1a89
junderw <junderwood@bitcoinbank.co.jp>
2019-02-28 11:30:31 +0900

    Add Typescript into the repo and commit JS into src directory

---
+++ src/crypto.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const createHash = require('create-hash');
+const createHmac = require('create-hmac');
+function hash160(buffer) {
+    return createHash('rmd160').update(createHash('sha256').update(buffer).digest()).digest();
+}
+exports.hash160 = hash160;
+function hmacSHA512(key, data) {
+    return createHmac('sha512', key).update(data).digest();
+}
+exports.hmacSHA512 = hmacSHA512;
---
+++ ts-src/crypto.ts
@@ -0,0 +1,12 @@
+const createHash = require('create-hash')
+const createHmac = require('create-hmac')
+
+export function hash160 (buffer: Buffer): Buffer {
+  return createHash('rmd160').update(
+    createHash('sha256').update(buffer).digest()
+  ).digest()
+}
+
+export function hmacSHA512 (key: Buffer, data: Buffer): Buffer {
+  return createHmac('sha512', key).update(data).digest()
+}
