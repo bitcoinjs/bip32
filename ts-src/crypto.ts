565b49ea611f2d3da477cfbf8a572c5374e1221b
junderw <junderwood@bitcoinbank.co.jp>
2019-03-29 12:37:15 +0900

    Fix ripemd160 issue with Electron

--- ts-src/crypto.ts
+++ ts-src/crypto.ts
@@ -1,8 +1,19 @@
 const createHash = require('create-hash');
 const createHmac = require('create-hmac');
 
+let ripemd160: string;
+try {
+  ripemd160 = require('crypto')
+    .getHashes()
+    .includes('rmd160')
+    ? 'rmd160'
+    : 'ripemd160';
+} catch (err) {
+  ripemd160 = 'rmd160';
+}
+
 export function hash160(buffer: Buffer): Buffer {
-  return createHash('rmd160')
+  return createHash(ripemd160)
     .update(
       createHash('sha256')
         .update(buffer)
