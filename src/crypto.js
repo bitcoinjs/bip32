36717bf8706b3ce5832717debabd57657d516998
junderw <junderwood@bitcoinbank.co.jp>
2019-04-05 12:23:07 +0900

    Fix rmd160 issue without using crypto

--- src/crypto.js
+++ src/crypto.js
@@ -2,23 +2,20 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const createHash = require('create-hash');
 const createHmac = require('create-hmac');
-let ripemd160;
-try {
-    ripemd160 = require('crypto')
-        .getHashes()
-        .includes('rmd160')
-        ? 'rmd160'
-        : 'ripemd160';
-}
-catch (err) {
-    ripemd160 = 'rmd160';
-}
 function hash160(buffer) {
-    return createHash(ripemd160)
-        .update(createHash('sha256')
+    const sha256Hash = createHash('sha256')
         .update(buffer)
-        .digest())
         .digest();
+    try {
+        return createHash('rmd160')
+            .update(sha256Hash)
+            .digest();
+    }
+    catch (err) {
+        return createHash('ripemd160')
+            .update(sha256Hash)
+            .digest();
+    }
 }
 exports.hash160 = hash160;
 function hmacSHA512(key, data) {
