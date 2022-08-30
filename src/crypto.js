efaef6fe40295cacd84ab5c806bec97f002f48d9
Arthur Kh <listbox6@gmail.com>
2022-08-30 22:14:05 +0300

    fix: Error: error:0308010C:digital envelope routines::unsupported with ripemd160 package

--- src/crypto.js
+++ src/crypto.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const createHash = require('create-hash');
 const createHmac = require('create-hmac');
+const Ripemd160 = require('ripemd160');
 function hash160(buffer) {
     const sha256Hash = createHash('sha256')
         .update(buffer)
@@ -12,9 +13,14 @@ function hash160(buffer) {
             .digest();
     }
     catch (err) {
-        return createHash('ripemd160')
-            .update(sha256Hash)
-            .digest();
+        try {
+            return createHash('ripemd160')
+                .update(sha256Hash)
+                .digest();
+        }
+        catch (e) {
+            return new Ripemd160().update(sha256Hash).digest();
+        }
     }
 }
 exports.hash160 = hash160;
--- ts-src/crypto.ts
+++ ts-src/crypto.ts
@@ -1,5 +1,6 @@
 const createHash = require('create-hash');
 const createHmac = require('create-hmac');
+const Ripemd160 = require('ripemd160');
 
 export function hash160(buffer: Buffer): Buffer {
   const sha256Hash: Buffer = createHash('sha256')
@@ -10,9 +11,13 @@ export function hash160(buffer: Buffer): Buffer {
       .update(sha256Hash)
       .digest();
   } catch (err) {
-    return createHash('ripemd160')
-      .update(sha256Hash)
-      .digest();
+    try {
+      return createHash('ripemd160')
+        .update(sha256Hash)
+        .digest();
+    } catch (e) {
+      return new Ripemd160().update(sha256Hash).digest();
+    }
   }
 }
 
